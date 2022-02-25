4033054a537e602b1282fa7dc767f526fef18bae
JudithJanse <59368871+JudithJanse@users.noreply.github.com>
2022-02-25 13:42:56 +0100

    book

--- SWE/Models/Book.cs
+++ SWE/Models/Book.cs
@@ -8,6 +8,11 @@ namespace SWE.Models
 {
     internal class Book
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Author Author { get; set; }
+
+
 
     }
 }
