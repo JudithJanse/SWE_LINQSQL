20297ca26c5d300b63af1d0976ff725ea8cd7e61
JudithJanse <59368871+JudithJanse@users.noreply.github.com>
2022-03-14 10:57:35 +0100

    controller weg, CS0053

---
+++ SWE/Models/DbRepo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWE.Models
+{
+    public class DbRepo : IRepo
+    {
+        private readonly LibraryContext _db;
+
+        public DbRepo(LibraryContext db)
+        {
+            _db = db;
+        }
+
+        public IQueryable<Book> Books => _db.Books;
+        public IQueryable<Author> Authors => _db.Authors;
+
+        public void Add<EntityType>(EntityType entity) => _db.Add(entity);
+        public void SaveChanges() => _db.SaveChanges();
+    }
+}
---
+++ SWE/Models/IRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWE.Models
+{
+    public interface IRepo
+    {
+        public IQueryable<Book> Books { get; }
+
+        public IQueryable<Author> Authors {get;}
+
+        void Add<Entitytype>(Entitytype entity);
+        void SaveChanges();
+
+
+    }
+}
