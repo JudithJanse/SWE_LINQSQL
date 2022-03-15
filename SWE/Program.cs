e383a57eb0d274f667ed9b6fa3f1a28288facf77
Jennifer2305 <57941124+Jennifer2305@users.noreply.github.com>
2022-03-15 02:28:16 +0100

    in memory database connectie voortgang denk ik

--- SWE/Program.cs
+++ SWE/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SWE.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+var options = new DbContextOptionsBuilder<LibraryContext>()
+   .UseInMemoryDatabase(databaseName: "InMemoryDB")
+   .Options;
+
+using (var context = new LibraryContext(options))
+{
+    var author = new Author
+    {
+        Name = "Geronimo Stilton",
+    };
+    var book = new Book
+    {
+        Title = "Fantasia",
+        Author = author
+    };
+
+    context.Books.Add(book);
+    context.SaveChanges();
+
+}
